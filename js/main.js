a98bcf7be0ae02602189bac18e668c040a7fb9e4
Qayumov Pahlavon <qayumovpahlavon2778tatu@gmail.com>
2022-11-03 10:35:09 +0500

    Added javascript files

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
 // Global variables
 const canvas = document.querySelector("canvas");
 const toolBtns = document.querySelectorAll(".tool");
+const fillColor = document.querySelector("#fill-color");
+const sizeSlider = document.querySelector("#size-slider");
+const colorBtns = document.querySelectorAll(".colors .option");
+const colorPicker = document.querySelector("#color-picker");
+const clearCanvasBtn = document.querySelector(".clear-canvas");
+const saveImageBtn = document.querySelector(".save-img");
 
 // Variable
 let ctx = canvas.getContext("2d"),
   isDrawing = false,
   brushWidth = 5,
   selectedTool = "brush",
+  selectedColor = "#000",
   prevMouseX,
   prevMouseY,
   snapshot;
+
+const setCannvasBackground = () => {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = selectedColor;
+};
 
 // Set canvas width and height
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
+  setCannvasBackground();
 });
 
 // Start drawing
@@ -24,11 +38,23 @@ const startDraw = (e) => {
   prevMouseY = e.offsetY;
   ctx.beginPath();
   ctx.lineWidth = brushWidth;
+  ctx.strokeStyle = selectedColor;
+  ctx.fillStyle = selectedColor;
   snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
+// rectangle
 const drawRectangle = (e) => {
-  ctx.strokeRect(
+  if (!fillColor.checked) {
+    return ctx.strokeRect(
+      e.offsetX,
+      e.offsetY,
+      prevMouseX - e.offsetX,
+      prevMouseY - e.offsetY
+    );
+  }
+
+  ctx.fillRect(
     e.offsetX,
     e.offsetY,
     prevMouseX - e.offsetX,
@@ -36,19 +62,49 @@ const drawRectangle = (e) => {
   );
 };
 
+//circle
+const drawCircle = (e) => {
+  ctx.beginPath();
+  const radius =
+    Math.sqrt(Math.pow(prevMouseX - e.offsetX, 2)) +
+    Math.pow(prevMouseY - e.offsetY, 2);
+  ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+// triangle
+const drawTriangle = (e) => {
+  ctx.beginPath();
+  ctx.moveTo(prevMouseX, prevMouseY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+  ctx.closePath();
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+// drawing
 const drawing = (e) => {
   if (!isDrawing) return;
   ctx.putImageData(snapshot, 0, 0);
 
+  if (selectedTool == "brush" || selectedTool == "eraser") {
+    ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+  }
   switch (selectedTool) {
-    case "brush":
-      ctx.lineTo(e.offsetX, e.offsetY);
-      ctx.stroke();
-      break;
-
     case "rectangle":
       drawRectangle(e);
       break;
+
+    case "circle":
+      drawCircle(e);
+      break;
+
+    case "triangle":
+      drawTriangle(e);
+      break;
+
     default:
       break;
   }
@@ -61,6 +117,36 @@ toolBtns.forEach((btn) => {
     selectedTool = btn.id;
     console.log(`Selected tool ${selectedTool}`);
   });
+});
+
+sizeSlider.addEventListener("change", () => (brushWidth = sizeSlider.value));
+
+colorBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    document.querySelector(".options .selected").classList.remove("selected");
+    btn.classList.add("selected");
+    const bgColor = window
+      .getComputedStyle(btn)
+      .getPropertyValue("background-color");
+    selectedColor = bgColor;
+  });
+});
+
+colorPicker.addEventListener("change", () => {
+  colorPicker.parentElement.style.background = colorPicker.value;
+  colorPicker.parentElement.click();
+});
+
+clearCanvasBtn.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  setCannvasBackground();
+});
+
+saveImageBtn.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.download = `Paint-design${Date.now()}.jpg`;
+  link.href = canvas.toDataURL();
+  link.click();
 });
 
 const stopDraw = () => {
